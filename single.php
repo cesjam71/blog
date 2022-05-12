0df1c6a4acc36099631d51813f997adfb4080756
Cesar Aquino <correofalso@cesar.pe>
2022-05-11 21:59:07 -0500

    custom fields

--- single.php
+++ single.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
 	<section class="container">
 
+		<h1><?php the_title(); ?></h1>
+
+
+		<?php the_field('resumen'); ?>
 
 		<?php the_post_thumbnail("large") ?>
 
-		<h1><?php the_title(); ?></h1>
 
-		<?php the_title(); ?>
+		<?php the_content(); ?>
+
+		<h4>Fuente: XXXXXX</h4>
 			
 
 
